f401229a4200c30674d90b3b3b40a2c11ae4164a
wencywonderx <s2258422@ed.ac.uk>
2022-07-08 22:33:38 +0100

    test

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/test_folder/test_file.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/test_folder/test_file.py
@@ -1,4 +1,16 @@
-from fastpitch.prepare_dataset import TTSDataset
+import functools
+import re
+from pathlib import Path
+
+import numpy as np
+import torch
+from scipy import ndimage
+from scipy.stats import betabinom
+
+import common.layers as layers
+from common.text.text_processing import TextProcessing
+from common.utils import load_wav_to_torch, load_filepaths_and_text, to_gpu
+from fastpitch.pitch_things import interpolate_f0, estimate_pitch, normalize_pitch
 
 
 test = TTSDataset(
