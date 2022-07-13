4115694994aa2456fef2ac7635aae54b42924b51
wencywonderx <s2258422@ed.ac.uk>
2022-07-13 20:38:46 +0100

    fix parser for delta f0

--- PyTorch/SpeechSynthesis/FastPitch/models.py
+++ PyTorch/SpeechSynthesis/FastPitch/models.py
@@ -133,6 +133,17 @@ def get_model_config(model_name, args):
             pitch_predictor_n_layers=args.pitch_predictor_n_layers,
             # pitch conditioning
             pitch_embedding_kernel_size=args.pitch_embedding_kernel_size,
+           
+            #-----------------------------------added by me-------------------------------
+            # delta f0 predictor
+            delta_f0_predictor_kernel_size=args.delta_f0_predictor_kernel_size,
+            delta_f0_predictor_filter_size=args.delta_f0_predictor_filter_size,
+            p_delta_f0_predictor_dropout=args.p_delta_f0_predictor_dropout,
+            delta_f0_predictor_n_layers=args.pdelta_f0_predictor_n_layers,
+            # delta f0 conditioning
+            delta_f0_embedding_kernel_size=args.delta_f0_embedding_kernel_size,    
+            #------------------------------------------------------------------------------
+                   
             # speakers parameters
             n_speakers=args.n_speakers,
             speaker_emb_weight=args.speaker_emb_weight,
