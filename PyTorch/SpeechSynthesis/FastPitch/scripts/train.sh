0d1d40d7a5e0f3107d02dbc00ce0ab91c645a3ee
wencywonderx <s2258422@ed.ac.uk>
2022-07-17 17:36:09 +0100

    crashing

--- PyTorch/SpeechSynthesis/FastPitch/scripts/train.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/train.sh
@@ -92,7 +92,7 @@ ARGS+=" --n-speakers $NSPEAKERS"
 [ "$INTERPOLATE" = true ]          && ARGS+=" --interpolate-f0"
 [ "$MEAN_DELTA" = true ]           && ARGS+=" --mean-and-delta-f0"
 # [ "$MEAN_DELTA" = true ]           && ARGS+=" --delta-f0-conditioning"
-[ "$NARMAL" = true ]               && ARGS+=" --normal-f0"
+# [ "$NARMAL" = true ]               && ARGS+=" --normal-f0"
 #------------------------------------------------------------------------------
 
 if [ "$SAMPLING_RATE" == "44100" ]; then
