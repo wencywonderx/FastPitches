192ecaedc3f3e8600351ada843362c3ab12e9602
wencywonderx <s2258422@ed.ac.uk>
2022-07-18 16:18:17 +0100

    same

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
@@ -270,6 +270,7 @@ class TTSDataset(torch.utils.data.Dataset):
                 pitch = torch.from_numpy(pitch).unsqueeze(0)
                 # print("\n convert to pitch tensor\n", pitch)
             if self.pitch_mean is not None:
+                print("!!!!!!!!!!!!!!doing normalization")
                 assert self.pitch_std is not None
                 pitch = normalize_pitch(pitch, self.pitch_mean, self.pitch_std)                
             if self.mean_and_delta_f0:
