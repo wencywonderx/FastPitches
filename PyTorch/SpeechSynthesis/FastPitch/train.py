f6c35bc752c0ef896fe6e105ddd732c74f3a686f
wencywonderx <s2258422@ed.ac.uk>
2022-07-30 00:40:36 +0100

    same

--- PyTorch/SpeechSynthesis/FastPitch/train.py
+++ PyTorch/SpeechSynthesis/FastPitch/train.py
@@ -315,13 +315,13 @@ def plot_mels(pred_tgt_lists):
         #     mel, pitch, energy, slope_f0 = local_prep_tgts[i]
         #------------------------------------------------------
         # pitch = pitch * pitch_std + pitch_mean
-        # axes[i][0].imshow(mel, origin="lower")
-        # axes[i][0].set_aspect(2.5, adjustable="box")
-        # axes[i][0].set_ylim(0, mel.shape[0])
-        # axes[i][0].set_title(titles[i], fontsize="medium")
-        # axes[i][0].tick_params(labelsize="x-small", left=False,
-        #                        labelleft=False)
-        # axes[i][0].set_anchor("W")
+        axes[i][0].imshow(mel, origin="lower")
+        axes[i][0].set_aspect(2.5, adjustable="box")
+        axes[i][0].set_ylim(0, mel.shape[0])
+        axes[i][0].set_title(titles[i], fontsize="medium")
+        axes[i][0].tick_params(labelsize="x-small", left=False,
+                               labelleft=False)
+        axes[i][0].set_anchor("W")
 
         # ax1 = add_axis(fig, axes[i][0])
         # ax1.plot(pitch, color="tomato")
