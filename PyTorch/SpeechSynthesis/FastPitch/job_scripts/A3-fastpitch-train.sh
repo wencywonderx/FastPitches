fcbc609e25da04ea574144c41428b9c69bd2e86e
WANG XI <s2258422@ed.ac.uk>
2022-07-29 13:10:31 +0100

    eddie

--- PyTorch/SpeechSynthesis/FastPitch/job_scripts/A3-fastpitch-train.sh
+++ PyTorch/SpeechSynthesis/FastPitch/job_scripts/A3-fastpitch-train.sh
@@ -44,7 +44,7 @@ export DATASET_PATH=$SCRATCH/LJSpeech-1.1
 # which point to saved pitch contours. If extracting pitches from audio
 # online with PITCH_ONLINE_DIR set below, use ljs_audio_text_*.txt files
 
-export TRAIN_FILELIST=$FP/filelists/ljs_audio_pitch_text_train_v3.txt ---------------------------------------changed
+export TRAIN_FILELIST=$FP/filelists/ljs_audio_pitch_text_train_v3.txt #---------------------------------------changed
 export VAL_FILELIST=$FP/filelists/ljs_audio_pitch_text_val.txt
 # export TRAIN_FILELIST=$FP/filelists/test_file_100.txt
 # export VAL_FILELIST=$FP/filelists/val_file_10.txt
