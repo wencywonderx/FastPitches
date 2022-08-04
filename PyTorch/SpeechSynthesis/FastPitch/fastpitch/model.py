93206e0d3be9dfaefb3ea309bc079b98ed8469be
wencywonderx <s2258422@ed.ac.uk>
2022-08-04 22:06:28 +0100

    s

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/model.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/model.py
@@ -510,8 +510,8 @@ class FastPitch(nn.Module):
             delta_f0_pred = self.delta_f0_predictor(enc_out, enc_mask).permute(0, 2, 1)
             input = enc_out * enc_mask
             mean_f0_pred = self.mean_f0_predictor(input)
-            print(f'this is predicted mean f0 {mean_f0_pred}')
-            print(f'this is predicted delta f0 {delta_f0_pred}')
+            print(f'this is predicted mean f0 {mean_f0_pred.shape}')
+            print(f'this is predicted delta f0 {delta_f0_pred.shape}')
             mean_and_delta_f0_pred = delta_f0_pred + mean_f0_pred.view(mean_f0_pred.size(0), 1, 1)
             print(f'this is predicted mean and delta f0 {mean_and_delta_f0_pred}')
             if mean_f0_tgt is None and delta_f0_tgt is None:
