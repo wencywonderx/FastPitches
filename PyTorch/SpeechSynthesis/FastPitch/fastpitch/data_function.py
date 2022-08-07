4b455f309655ed13b445b13ecd7a2acdb5b63893
wencywonderx <s2258422@ed.ac.uk>
2022-08-07 13:22:36 +0100

    s

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
@@ -185,7 +185,9 @@ class TTSDataset(torch.utils.data.Dataset):
             slope_f0 = None
             slope_delta = None
             mean_f0 = None
-            delta_f0 = None              
+            delta_f0 = None
+            print(f'pitch {pitch}')        
+            print(f'range_f0 {range_f0}')      
         else:
             print("!!!!!!!!!!!!!!!!!!!!!!!!!! not range")
             if self.mean_and_delta_f0:
