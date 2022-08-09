69a2488fe6b8c9f9c8277f49fa5ce0f75774c699
wencywonderx <s2258422@ed.ac.uk>
2022-08-09 12:13:06 +0100

    s

--- PyTorch/SpeechSynthesis/FastPitch/inference.py
+++ PyTorch/SpeechSynthesis/FastPitch/inference.py
@@ -251,9 +251,9 @@ def prepare_input_sequence(fields, device, symbol_set, text_cleaners, # encode t
             elif f == 'pitch' and load_pitch:
                 batch[f] = pad_sequence(batch[f], batch_first=True)
             # #-----------------------added--------------------------
-            # elif f == 'mean_f0':
-            #     # print("triggered!")
-            #     batch[f] = pad_sequence(batch[f], batch_first=True)
+            elif f == 'mean_f0':
+                # print("triggered!")
+                batch[f] = pad_sequence(batch[f], batch_first=True)
             # #------------------------------------------------------
             if type(batch[f]) is torch.Tensor:
                 batch[f] = batch[f].to(device)
